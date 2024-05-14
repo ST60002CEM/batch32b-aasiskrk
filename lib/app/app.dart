670dfcc2531d0f58136b60d07d34291ee0b8265b
aasiskrk <aasis.krk1@gmail.com>
2024-05-14 22:10:02 +0545

    Updated themes to change some parts according to the theme of the device, made login page format

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -6,8 +6,22 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.white,
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: const Color.fromRGBO(23, 23, 23, 1),
+        cardColor: const Color.fromRGBO(0, 0, 0, 1),
+
+        useMaterial3: true,
+        // colorSchemeSeed: Colors.lightGreen
+      ),
       home: LoginScreen(),
     );
   }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,68 +1,73 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final _gap = SizedBox(
-    height: 20,
-  );
   TextEditingController emailFieldController = TextEditingController();
-
   TextEditingController passwordFieldController = TextEditingController();
-
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: const Color.fromRGBO(73, 78, 83, 100),
+        backgroundColor: const Color.fromRGBO(27, 27, 27, 1),
         resizeToAvoidBottomInset: false,
         body: Form(
           key: _formKey,
           child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                _gap,
-                _gap,
-                _gap,
-                _gap,
-                _gap,
-                _gap,
-                _gap,
-                _gap,
-                _gap,
-                _gap,
-                SizedBox(
-                  width: 100,
-                  height: 100,
-                  child: Image.asset('assets/images/logo.png'),
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: Image.asset(
+                      'assets/images/logowhite.png',
+                    ),
+                  ),
                 ),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Color.fromRGBO(0, 0, 0, 100),
+                Expanded(
+                  flex: 5,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Theme.of(context).cardColor),
+                    child: const Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        // mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(left: 20),
+                            child: Text(
+                              "Welcome",
+                              style: TextStyle(
+                                fontSize: 32,
+                                fontFamily: "Epilogue",
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: -0.3,
+                              ),
+                            ),
+                          ),
+                          // Add other login elements here
+                        ],
+                      ),
+                    ),
                   ),
-                  height: 445,
-                  width: MediaQuery.of(context).size.width,
-                  child: const Column(
-                    children: [
-                      Text(
-                        "Welcome",
-                        style: TextStyle(
-                            fontSize: 32,
-                            fontFamily: "Epilogue",
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: -0.3),
-                      )
-                    ],
-                  ),
+                ),
+                TextFormField(
+                  validator: (value) {},
                 )
               ],
             ),

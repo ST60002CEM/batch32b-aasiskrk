b18bd54389756d71f1bde0ad772b246dbb5b3ca5
aasiskrk <aasiskrk1@gmail.com>
2024-05-29 19:04:08 +0545

    Changed progress indicator in splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -25,37 +25,53 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  flex: 2,
-                  child:
-                      Container(), // Adding an empty container to create space
+        body: Stack(
+          children: [
+            Center(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child:
+                          Container(), // Adding an empty container to create space
+                    ),
+                    Expanded(
+                      flex: 2, // Adjust this ratio as needed
+                      child: Image.asset(
+                        Theme.of(context).brightness == Brightness.dark
+                            ? 'assets/images/logowhite.png'
+                            : 'assets/images/logoblack.png',
+                        // Add margin to move the logo down
+                      ),
+                    ),
+                    Expanded(
+                      flex: 5, // Adjust this ratio as needed
+                      child: Image.asset(
+                        'assets/images/splashscreen.gif',
+                        // Set width to match the screen width
+                        height: 450,
+                        width: MediaQuery.of(context).size.width,
+                      ),
+                    ),
+                  ],
                 ),
-                Expanded(
-                  flex: 2, // Adjust this ratio as needed
-                  child: Image.asset(
-                    'assets/images/logowhite.png',
-                    // Add margin to move the logo down
-                  ),
-                ),
-                Expanded(
-                  flex: 5, // Adjust this ratio as needed
-                  child: Image.asset(
-                    'assets/images/splashscreen.gif',
-                    // Set width to match the screen width
-                    height: 450,
-                    width: MediaQuery.of(context).size.width,
-                  ),
-                ),
-                CircularProgressIndicator()
-              ],
+              ),
             ),
-          ),
+            Align(
+              alignment: Alignment.topRight,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: CircularProgressIndicator(
+                  backgroundColor: Colors.grey,
+                  strokeWidth: 4,
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );

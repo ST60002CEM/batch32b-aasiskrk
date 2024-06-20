0cd19d98f4c229853076d20ebd33c6f512812fcb
aasiskrk <aasiskrk1@gmail.com>
2024-06-20 17:55:57 +0545

    auto generated files

---
+++ lib/features/auth/data/model/auth_api_model.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'auth_api_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AuthApiModel _$AuthApiModelFromJson(Map<String, dynamic> json) => AuthApiModel(
+      id: json['_id'] as String?,
+      fullname: json['fullName'] as String,
+      email: json['email'] as String,
+      address: json['address'] as String,
+      password: json['password'] as String?,
+    );
+
+Map<String, dynamic> _$AuthApiModelToJson(AuthApiModel instance) =>
+    <String, dynamic>{
+      '_id': instance.id,
+      'fullName': instance.fullname,
+      'email': instance.email,
+      'address': instance.address,
+      'password': instance.password,
+    };

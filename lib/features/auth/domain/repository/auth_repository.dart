1d188bc1eab9a969a3fa9eaa5f0931038d399322
aasiskrk <aasiskrk1@gmail.com>
2024-06-20 17:58:27 +0545

    auth repository to have remote repository provider

--- lib/features/auth/domain/repository/auth_repository.dart
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -3,14 +3,15 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../../core/failure/failure.dart';
 import '../../data/repository/auth_local_repository.dart';
+import '../../data/repository/auth_remote_repository.dart';
 import '../entity/auth_entity.dart';
 
 final authRepositoryProvider = Provider<IAuthRepository>((ref) {
-  return ref.read(authLocalRepositoryProvider);
+  return ref.read(authRemoteRepositoryProvider);
 });
 
 abstract class IAuthRepository {
-  Future<Either<Failure, bool>> registerUser(AuthEntity student);
+  Future<Either<Failure, bool>> registerUser(AuthEntity user);
   Future<Either<Failure, bool>> loginUser(String email, String password);
   Future<Either<Failure, String>> uploadProfilePicture(File file);
 }

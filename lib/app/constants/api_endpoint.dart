8bc5809700f7ba4afef5b48d64098c14a7bc6b37
aasiskrk <aasiskrk1@gmail.com>
2024-06-23 21:48:20 +0545

    changed baseurl

--- lib/app/constants/api_endpoint.dart
+++ lib/app/constants/api_endpoint.dart
@@ -3,7 +3,7 @@ class ApiEndpoints {
 
   static const Duration connectionTimeout = Duration(seconds: 1000);
   static const Duration receiveTimeout = Duration(seconds: 1000);
-  static const String baseUrl = "http://10.0.2.2:5000/api/";
+  static const String baseUrl = "http://192.168.1.66:5000/api/";
   //static const String baseUrl = "http://localhost:3000/api/v1/";
   //static const String baseUrl = "http://192.168.4.4:3000/api/v1/";
 

8f59764a2f7352183392fbe73798284d958cce3b
aasiskrk <aasiskrk1@gmail.com>
2024-06-29 23:29:44 +0545

    updated

--- lib/features/auth/domain/repository/auth_repository.dart
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -1,12 +1,19 @@
 import 'dart:io';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:playforge/core/common/internet_checker/internet_checker_view_model.dart';
+import 'package:playforge/features/auth/data/repository/auth_local_repository.dart';
 import '../../../../core/failure/failure.dart';
 import '../../data/repository/auth_remote_repository.dart';
 import '../entity/auth_entity.dart';
 
 final authRepositoryProvider = Provider<IAuthRepository>((ref) {
-  return ref.read(authRemoteRepositoryProvider);
+  final checkConnectivity = ref.read(connectivityStatusProvider);
+  if (checkConnectivity == ConnectivityStatus.isConnected) {
+    return ref.read(authRemoteRepositoryProvider);
+  } else {
+    return ref.read(authLocalRepositoryProvider);
+  }
 });
 
 abstract class IAuthRepository {

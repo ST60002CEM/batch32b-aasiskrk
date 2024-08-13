834481e04c38b5cd36a12eef05188ebcd7a14908
aasiskrk <aasiskrk1@gmail.com>
2024-08-13 23:47:13 +0545

    Forum viewmodel

--- lib/features/dashboard/presentation/viewmodel/forum_view_model.dart
+++ lib/features/dashboard/presentation/viewmodel/forum_view_model.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../../core/common/my_snackbar.dart';
@@ -14,54 +16,34 @@ class ForumViewModel extends StateNotifier<ForumState> {
   final ForumUseCase forumUseCase;
 
   ForumViewModel(this.forumUseCase) : super(ForumState.initial()) {
-    getAllForumPosts();
+    getAllPosts();
   }
 
   Future resetState() async {
     state = ForumState.initial();
-    getAllForumPosts();
+    getAllPosts();
   }
 
-  Future<void> addForumPost(ForumPostEntity post) async {
-    state = state.copyWith(isLoading: true);
-    var result = await forumUseCase.addForumPost(post);
-    result.fold(
-      (failure) {
-        state = state.copyWith(isLoading: false, error: failure.error);
-        showMySnackBar(message: failure.error, color: Colors.red);
-      },
-      (success) {
-        state = state.copyWith(isLoading: false, error: null);
-        showMySnackBar(message: "Post added successfully");
-        getAllForumPosts();
-      },
-    );
-  }
-
-  Future<void> getAllForumPosts() async {
-    if (state.isLoading || state.hasReachedMax) return;
+  Future<void> getAllPosts({int? page}) async {
     state = state.copyWith(isLoading: true);
     final currentState = state;
-    final page = currentState.page + 1;
+    final currentPage = page ?? currentState.page + 1;
     final posts = currentState.posts;
     final hasReachedMax = currentState.hasReachedMax;
+
     if (!hasReachedMax) {
-      var result = await forumUseCase.getAllForumPosts(page);
+      final result = await forumUseCase.getAllForumPosts(currentPage);
       result.fold(
-        (failure) {
-          state = state.copyWith(
-              isLoading: false, error: failure.error, hasReachedMax: true);
-          showMySnackBar(message: failure.error, color: Colors.red);
-        },
+        (failure) =>
+            state = state.copyWith(hasReachedMax: true, isLoading: false),
         (data) {
           if (data.isEmpty) {
             state = state.copyWith(hasReachedMax: true, isLoading: false);
           } else {
             state = state.copyWith(
+              posts: currentPage == 1 ? data : [...posts, ...data],
+              page: currentPage,
               isLoading: false,
-              error: null,
-              posts: [...posts, ...data],
-              page: page,
             );
           }
         },
@@ -69,17 +51,32 @@ class ForumViewModel extends StateNotifier<ForumState> {
     }
   }
 
-  Future<void> getForumPost(String postId) async {
+  Future<void> getUsersPosts(String userId) async {
     state = state.copyWith(isLoading: true);
-    var result = await forumUseCase.getForumPost(postId);
+
+    final result = await forumUseCase.getForumPost(userId);
     result.fold(
-      (failure) {
-        state = state.copyWith(isLoading: false, error: failure.error);
-        showMySnackBar(message: failure.error, color: Colors.red);
+      (failure) => state = state.copyWith(isLoading: false),
+      (data) {
+        if (data.isEmpty) {
+          state = state.copyWith(isLoading: false);
+        } else {
+          state = state.copyWith(
+            userPosts: data,
+            isLoading: false,
+          );
+        }
       },
-      (post) {
-        state = state.copyWith(isLoading: false, error: null, posts: [post!]);
-      },
+    );
+  }
+
+  // Add the searchAllPosts method here
+  Future<void> searchPosts(String searchTerm) async {
+    state = state.copyWith(isLoading: true);
+    final result = await forumUseCase.searchAllPosts(searchTerm);
+    result.fold(
+      (failure) => state = state.copyWith(isLoading: false, searchResults: []),
+      (posts) => state = state.copyWith(isLoading: false, searchResults: posts),
     );
   }
 }

88e9a09a8818b2d066fa02aac6d50667f0dd1be3
aasiskrk <aasiskrk1@gmail.com>
2024-08-13 23:47:58 +0545

    edited

--- lib/features/dashboard/domain/repository/forum_repository.dart
+++ lib/features/dashboard/domain/repository/forum_repository.dart
@@ -1,22 +1,28 @@
+import 'dart:io';
+
 import 'package:dartz/dartz.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:playforge/core/common/internet_checker/internet_checker_view_model.dart';
 import '../../../../core/failure/failure.dart';
+import '../../../../core/failure/post_failure.dart';
 import '../../data/repository/forum_local_repository.dart';
 import '../../data/repository/forum_remote_repository.dart';
 import '../entity/forum_entity.dart';
 
 final forumRepositoryProvider = Provider<IForumRepository>((ref) {
   final checkConnectivity = ref.read(connectivityStatusProvider);
-  if (checkConnectivity == ConnectivityStatus.isConnected) {
-    return ref.read(forumRemoteRepositoryProvider);
-  } else {
-    return ref.read(forumLocalRepositoryProvider);
-  }
+  // if (checkConnectivity == ConnectivityStatus.isConnected) {
+  //   return ref.read(forumRemoteRepositoryProvider);
+  // } else {
+  //   return ref.read(forumLocalRepositoryProvider);
+  // }
+  return ref.read(forumRemoteRepositoryProvider);
 });
 
 abstract class IForumRepository {
-  Future<Either<Failure, bool>> addForumPost(ForumPostEntity post);
+  Future<Either<Failure, bool>> addForumPost(ForumPostEntity post, File? image);
   Future<Either<Failure, List<ForumPostEntity>>> getAllForumPosts(int page);
-  Future<Either<Failure, ForumPostEntity?>> getForumPost(String postId);
+  Future<Either<Failure, List<ForumPostEntity>>> getForumPost(String postId);
+  Future<Either<PostFailure, List<ForumPostEntity>>> searchAllPost(
+      String searchTerm);
 }
--- lib/features/dashboard/presentation/state/forum_state.dart
+++ lib/features/dashboard/presentation/state/forum_state.dart
@@ -4,15 +4,19 @@ class ForumState {
   final bool isLoading;
   final String? error;
   final List<ForumPostEntity> posts;
+  final List<ForumPostEntity> userPosts;
+  final List<ForumPostEntity> searchResults;
   final bool hasReachedMax;
   final int page;
 
   ForumState({
     required this.isLoading,
     this.error,
+    required this.searchResults,
     required this.posts,
     required this.page,
     required this.hasReachedMax,
+    required this.userPosts,
   });
 
   factory ForumState.initial() {
@@ -20,8 +24,10 @@ class ForumState {
       isLoading: false,
       error: null,
       posts: [],
+      searchResults: [],
       hasReachedMax: false,
       page: 0,
+      userPosts: [],
     );
   }
 
@@ -30,7 +36,9 @@ class ForumState {
     String? error,
     int? page,
     List<ForumPostEntity>? posts,
+    List<ForumPostEntity>? searchResults,
     bool? hasReachedMax,
+    List<ForumPostEntity>? userPosts,
   }) {
     return ForumState(
       isLoading: isLoading ?? this.isLoading,
@@ -38,10 +46,8 @@ class ForumState {
       error: error ?? this.error,
       page: page ?? this.page,
       posts: posts ?? this.posts,
+      userPosts: userPosts ?? this.userPosts,
+      searchResults: searchResults ?? this.searchResults,
     );
   }
-
-  @override
-  String toString() =>
-      'ForumState(isLoading: $isLoading, error: $error, posts: $posts, hasReachedMax: $hasReachedMax, page: $page)';
 }

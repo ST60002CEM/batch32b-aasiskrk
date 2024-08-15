57aaa62738f93ee48be7816ec1ada78d8be661ba
aasiskrk <aasiskrk1@gmail.com>
2024-08-15 16:13:33 +0545

    edited

--- lib/features/dashboard/data/repository/forum_remote_repository.dart
+++ lib/features/dashboard/data/repository/forum_remote_repository.dart
@@ -4,6 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../../core/failure/failure.dart';
 import '../../../../core/failure/post_failure.dart';
+import '../../domain/entity/comment_entity.dart';
 import '../../domain/entity/forum_entity.dart';
 import '../../domain/repository/forum_repository.dart';
 import '../../../dashboard/data/data_source/remote/forum_remote_data_source.dart';
@@ -36,13 +37,50 @@ class ForumRemoteRepository implements IForumRepository {
   }
 
   @override
-  Future<Either<Failure, List<ForumPostEntity>>> getForumPost(String postId) {
-    return _forumRemoteDataSource.getSingleForumPost(postId);
+  Future<Either<Failure, List<ForumPostEntity>>> getForumPost() {
+    return _forumRemoteDataSource.getUserForumPost();
   }
 
   @override
   Future<Either<PostFailure, List<ForumPostEntity>>> searchAllPost(
       String searchTerm) {
     return _forumRemoteDataSource.searchALLPost(searchTerm);
+  }
+
+  @override
+  Future<Either<Failure, ForumPostEntity>> getPosts(String userId) {
+    return _forumRemoteDataSource.getPost(userId);
+  }
+
+  @override
+  Future<Either<Failure, bool>> likePost(String postId) {
+    return _forumRemoteDataSource.likePost(postId);
+  }
+
+  @override
+  Future<Either<Failure, bool>> dislikePost(String postId) {
+    return _forumRemoteDataSource.dislikePost(postId);
+  }
+
+  @override
+  Future<Either<Failure, bool>> addComment(
+      String postId, AddCommentEntity commentEntity) {
+    return _forumRemoteDataSource.addComment(postId, commentEntity);
+  }
+
+  @override
+  Future<Either<Failure, bool>> editPost(
+      String postId, ForumPostEntity updatedPostEntity) {
+    return _forumRemoteDataSource.editPost(postId, updatedPostEntity);
+  }
+
+  @override
+  Future<Either<Failure, bool>> deletePost(String postId) {
+    return _forumRemoteDataSource.deletePost(postId);
+  }
+
+  @override
+  Future<Either<Failure, bool>> viewPost(String postId) {
+    return _forumRemoteDataSource.viewPost(postId);
   }
 }
